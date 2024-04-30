837c6999e055a56383caef8f9a9770735850b1d2
Miqayel <132817479+MikaPost@users.noreply.github.com>
2024-04-30 23:41:57 +0400

    pylint 9.58/10

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
+"""
+This file is for our new theme: word Frequency cointer
+Create by: Miqayel Postoyan
+Date: 30 April
+"""
 import re
 
-def get_argument(fname):
+def get_file(fname):
+    """
+    Function: get_file
+    Brief: read txt file
+    Params: file name
+    Return:	data file
+    """
     with open(fname) as f:
         punctuation_pattern = r'[^\w\s]'
         return re.sub(punctuation_pattern, "", f.read()).split()
 
 
 def count_occurrences(cnt):
+    """
+    Function: count_occurrences
+    Brief: count_occurrences
+    Params: data
+    Return:	ml list
+    """
     ml = {}
     for i in cnt:
         if i.isalpha():
@@ -18,13 +35,23 @@ def count_occurrences(cnt):
 
 
 def printt(mylist):
+    """
+    Function: printt
+    Brief: print list
+    Params: mylist
+    Return:	None
+    """
     for i in mylist:
         print(i[0], i[1])
 
 
 def main():
+    """
+    Function: main
+    Brief: Entry point
+    """
     fname = "a.txt"
-    cnt = get_argument(fname)
+    cnt = get_file(fname)
     mylist = count_occurrences(cnt)
     mylist = sorted(mylist.items(), key=lambda x: x[1], reverse=True)
     printt(mylist)
